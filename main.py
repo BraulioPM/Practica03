d289dbfe9240e739e4538901cfc83e18428c20c7
Juan <juangarpe94@gmail.com>
2015-04-14 16:54:36 +0200

    pracita3

--- main.py
+++ main.py
@@ -9,39 +9,47 @@ import ApiTwitter
 import twitter
 import io
 import json
+from flask import request
+app=Flask(__name__)
+GoogleMaps(app)
+global termino 
+termino=[]
 
 #Definimos con variables los datos que queremos obtener de twitter
 #es decir, los tweet que hablen del Atletico cerca de conil
-q="Atletico"
-geocode='36.2780990,-6.0862070,100km'
 
-twitter_api= ApiTwitter.oauth_login()
-tweets= twitter_api.search.tweets(q=q, geocode=geocode)
-ApiTwitter.save_json("Gtweets",tweets)
+@app.route("/buscar", methods=['POST'])
+def buscar():
+	termino = request.form['text'] 
 
-#Recuperamos el fichero json para trabajar con los datos obtenidos de twitter
-datos = json.loads(open('Gtweets.json').read())
-lista=[]
-for result in datos["statuses"]:
-    if result["coordinates"]:
-        xy=[result["coordinates"].values()[1][1],result["coordinates"].values()[1][0]]
-	lista.append(xy)
+	q=termino
+	geocode='36.2780990,-6.0862070,100km'
 
- 
-app=Flask(__name__)
-GoogleMaps(app)
+	twitter_api= ApiTwitter.oauth_login()
+	tweets= twitter_api.search.tweets(q=q, geocode=geocode)
+	ApiTwitter.save_json("Gtweets",tweets)
+
+	#Recuperamos el fichero json para trabajar con los datos obtenidos de twitter
+	datos = json.loads(open('Gtweets.json').read())
+	lista=[]
+	for result in datos["statuses"]:
+	    if result["coordinates"]:
+		xy=[result["coordinates"].values()[1][1],result["coordinates"].values()[1][0]]
+		lista.append(xy)
+	mymap = Map(
+		identifier="view-side",
+		lat=36.2780990,
+		lng=-6.0862070,
+		markers=lista,
+		style="height:700px; width:700px;margin:0;"
+	    )
+	return render_template('template.html', mymap=mymap)
+	 
 
 @app.route("/")
-def mapview():
-    
-    mymap = Map(
-        identifier="view-side",
-        lat=36.2780990,
-        lng=-6.0862070,
-        markers=lista,
-        style="height:700px; width:700px;margin:0;"
-    )
-    return render_template('template.html', mymap=mymap)
+def index():
+	return render_template('index.html')
+
 if __name__=="__main__":
     app.run(debug=True)
 
